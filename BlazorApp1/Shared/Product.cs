1b3d428394cbbe6499c46a35a1675fc1b5849b6a
asgharm <asgharm@enterprise.trin.net>
2023-06-27 15:38:35 -0500

    adding first class

---
+++ BlazorApp1/Shared/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+    }
+}
