dde3ffa650766b6b85438fd5b5caeb7215789ce0
asgharm <asgharm@enterprise.trin.net>
2023-06-28 09:57:08 -0500

    changing client side

--- BlazorApp1/Shared/Product.cs
+++ BlazorApp1/Shared/Product.cs
@@ -9,5 +9,12 @@ namespace BlazorApp1.Shared
     public class Product
     {
         public int Id { get; set; }
+
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+
+        public string ImageUrl { get; set; } = String.Empty;
+
+        public decimal Price { get; set; }
     }
 }
